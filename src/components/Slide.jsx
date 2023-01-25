7093ab4d4e2b5055513c7e4dadb01f0639ba10c2
ErJump <fedelijunior@gmail.com>
2023-01-25 14:11:50 +0100

    feat: :sparkles: Adds slide component

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Slide(props) {
   return (
-    <div className={'slide' + props.classes}>
+    <div className={'slide ' + props.classP}>
         <div className='review'>
             <h4>{props.slide.autore}</h4>
         </div>
